0a2a05d1a8b6f9c88ddc5169ee4be5f1ee2735f8
ashrrithsridhar <ashrrithsridhar@gmail.com>
2018-02-02 07:15:23 -0700

    Comments added to code

--- server.js
+++ server.js
@@ -1,64 +1,96 @@
-let express = require('express'),
-    bodyParser = require('body-parser'),
-    dotenv = require('dotenv').config(),
-    config = require('config'),
-    jwt = require('jsonwebtoken'),
-    helmet = require('helmet'),
-    app = express(),
-    swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json'),
-    logger = require(process.env.LOGS_DEFINITION_PATH || config.get('filePath').log_definition),
-    port = process.env.APP_PORT || config.get('app').port,
-    dbPort = process.env.DB_PORT || config.get('db').port,
-    dbName = process.env.DB_NAME || config.get('db').name,
-    dbHost = process.env.DB_HOST || config.get('db').host,
-    apiKey = process.env.API_SECRET_KEY,
-    // dbLink = 'mongodb://' + dbHost + '/' + dbName;
-    smartcarRouteFilePath = process.env.ROUTE_PATH || config.get('filePath').smartcarRoute,
-    smartcarControllerFilePath = process.env.CONTROLLER_PATH || config.get('filePath').smartcarController,
-    routes = require(smartcarRouteFilePath);
+/*
+ * Would have handled this more maturely if there were lot more environments to consider by creating a config.js file in the config folder
+ */
+if(process.env.NODE_ENV !== 'development' || process.env.NODE_ENV !== 'test') {
+    process.env.NODE_ENV = 'development';
+}
 
+'use strict';
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let dotenv = require('dotenv').config();
+let config = require('config');
+let jwt = require('jsonwebtoken');
+let helmet = require('helmet');
+let app = express();
+let swaggerUi = require('swagger-ui-express');
+let swaggerDocument = require('./swagger.json');
+let logger = require(process.env.LOGS_DEFINITION_PATH || config.get('filePath').log_definition);
+let port = process.env.APP_PORT || config.get('app').port;
+let dbPort = process.env.DB_PORT || config.get('db').port;
+let dbName = process.env.DB_NAME || config.get('db').name;
+let dbHost = process.env.DB_HOST || config.get('db').host;
+let apiKey = process.env.API_SECRET_KEY;
+let smartcarRouteFilePath = process.env.ROUTE_PATH || config.get('filePath').smartcarRoute;
+let smartcarControllerFilePath = process.env.CONTROLLER_PATH || config.get('filePath').smartcarController;
+let routes = require(smartcarRouteFilePath);
+
+/*
+ * Loading MongoDB instance using mongoose. Not in use for current design.
+ */
+
+//let dbLink = 'mongodb://' + dbHost + '/' + dbName;
 // console.log(process.env.API_SECRET_KEY);
 // console.log(dbPort);
 // console.log(port);
 // console.log(dbName);
 // console.log(dbHost);
 
-    /*var mongoClient = require('mongodb').MongoClient;
-    mongoClient.connect(dbLink)*/
+// var mongoClient = require('mongodb').MongoClient;
+// mongoClient.connect(dbLink)
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type : 'application/json' }));
+
+/*
+ * API documentation route
+ * http://localhost:3000/api-docs
+ * For now anyone can access this, but in the future I would be restricting its access based on Authorization Token
+ */
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', routes);
 
+/*
+ * Middleware for decoding users Authorization token
+ * Below method then makes a callback to the toke analyzer method in the controller section
+ */
+
 app.use(function (req, res, next) {
-    var token = req.body.token || req.headers || req.headers['x-access-token'];
+    let token = req.body.token || req.headers || req.headers['x-access-token'];
+    let user;
 
     if(token) {
         jwt.verify(token, apiKey, function(err, decoded) {
               if(err) {
-                  req.user = undefined;
+                  user = undefined;
               } else {
-                  req.user = decoded;
+                  user = decoded;
               }
 
               next();
         });
     } else {
-        req.user = undefined;
+        user = undefined;
         next();
     }
 });
 
 routes(app, smartcarControllerFilePath);
 
+/*
+ * Middleware for logging status messages in respective log files
+ */
 app.use(function(err, req, res, next) {
 
-    logger.error(err);
+    if(err.statusCode !== undefined && err.statusCode >= 200 && err.statusCode < 300) {
+        logger.log('info', err);
+    } else if(err.statusCode !== undefined){
+        logger.log('error', err);
+    }
 
     if (req.app.get('env') !== 'development' && req.app.get('env') !== 'test') {
         delete err.stack;
@@ -70,4 +102,7 @@ app.use(function(err, req, res, next) {
 app.listen(port);
 console.log("Smartcar API server started on port " + port);
 
+/*
+ * Exporting app for executing test scripts
+ */
 module.exports = app;
---
+++ test/gmVehicle.spec.js
@@ -0,0 +1,334 @@
+/*
+ * Contains test cases for all REST end points
+ * Followed the BDD approach for developing the REST end points
+ * The test scripts were developed using Chai framework
+ * Mocha was used to execute the test scripts
+ * The test scripts have been configured to run only in the test environment.
+ * A docker image has also been created for executing these test scripts.
+ */
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+let config = require('config'),
+    chai = require('chai'),
+    chaiHttp = require('chai-http'),
+    server = require(config.get('filePath').server);
+    should = chai.should(),
+    expect = chai.expect;
+
+chai.use(chaiHttp);
+
+/*
+ * Checks whether the error response json for all routes is according to standard
+ * The structure of the error response json is the same for all kinds of errors
+ * A standard error response helped in maintaining the logs in a more structured fashion
+ * The error codes include 400, 404, 422, 502, 500.
+ */
+let errorResponse = (response) => {
+    response.body.should.be.a('Object');
+
+    let length = Object.keys(response.body).length;
+
+    length.should.be.eql(4);
+    response.body.should.have.property('name');
+    response.body.should.have.property('message');
+    response.body.should.have.property('statusCode');
+    response.body.should.have.property('errorCode');
+
+    let name = response.body.name;
+    let message = response.body.message;
+    let statusCode = response.body.statusCode;
+    let errorCode = response.body.errorCode;
+
+    expect(name).to.be.a('string');
+    expect(message).to.be.a('string');
+    expect(statusCode).to.be.a('number');
+    expect(errorCode).to.be.a('number');
+};
+
+let energyResponse = (response) => {
+    response.body.should.be.an('Object');
+
+    let length = Object.keys(response.body).length;
+
+    length.should.be.eql(1);
+    response.body.should.have.property('percent');
+
+    let percent = response.body.percent;
+
+    expect(percent).to.be.a('number');
+}
+
+/*
+ * Test the /GET/vehicle/:id route
+ * The route has been tested for one successful case and for two error scenarios
+ */
+
+describe('/GET Vehicle Information', () => {
+    it('It should retreive the information of a vehicle given its ID', (done) => {
+          let vehicleID = '1234';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID)
+              .end((error, response) => {
+                  response.should.have.status(200);
+                  response.body.should.be.a('Object');
+
+                  let length = Object.keys(response.body).length;
+
+                  length.should.be.eql(4);
+                  response.body.should.have.property('vin');
+                  response.body.should.have.property('color');
+                  response.body.should.have.property('doorCount');
+                  response.body.should.have.property('driveTrain');
+
+                  let vin = response.body.vin;
+                  let color = response.body.color;
+                  let doorCount = response.body.doorCount;
+                  let driveTrain = response.body.driveTrain;
+
+                  expect(vin).to.be.a('string');
+                  expect(color).to.be.a('string');
+                  expect(driveTrain).to.be.a('string');
+                  expect(doorCount).to.be.a('number');
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not found', (done) => {
+          let vehicleID = '1236';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID)
+              .end((error, response) => {
+                  response.should.have.status(404);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not a valid number', (done) => {
+          let vehicleID = 'ash';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID)
+              .end((error, response) => {
+                  response.should.have.status(400);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+});
+
+/*
+ * Test the /GET/vehicle/:id/doors route
+ * The route has been tested for one successful case and for two error scenarios
+ */
+
+describe('/GET Vehicle Security Status', () => {
+    it('It should retreive the security status of a vehicle given its ID', (done) => {
+          let vehicleID = '1234';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/doors')
+              .end((error, response) => {
+                  response.should.have.status(200);
+                  response.body.should.be.an('array');
+
+                  expect(response.body).to.have.length.below(5);
+                  response.body.forEach((data) => {
+                      data.should.have.property('location');
+                      data.should.have.property('locked');
+
+                      let location = data.location;
+                      let locked = data.locked;
+
+                      expect(location).to.be.a('string');
+                      expect(locked).to.be.a('boolean');
+
+                  });
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not found', (done) => {
+          let vehicleID = '1236';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/doors')
+              .end((error, response) => {
+                  response.should.have.status(404);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not valid', (done) => {
+          let vehicleID = 'ash';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/doors')
+              .end((error, response) => {
+                  response.should.have.status(400);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+});
+
+/*
+ * Test the /GET/vehicle/:id/fuel route
+ * The route has been tested for one successful case and for two error scenarios
+ */
+
+describe('/GET Vehicle Fuel Level', () => {
+    it('It should retreive the fuel level of a vehicle given its ID', (done) => {
+          let vehicleID = '1234';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/fuel')
+              .end((error, response) => {
+                  response.should.have.status(200);
+                  energyResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not found', (done) => {
+          let vehicleID = '1236';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/fuel')
+              .end((error, response) => {
+                  response.should.have.status(404);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not valid', (done) => {
+          let vehicleID = 'ash';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/fuel')
+              .end((error, response) => {
+                  response.should.have.status(400);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+});
+
+/*
+ * Test the /GET/vehicle/:id/battery route
+ * The route has been tested for one successful case and for two error scenarios
+ */
+
+describe('/GET Vehicle Battery Level', () => {
+    it('It should retreive the battery level of a vehicle given its ID', (done) => {
+          let vehicleID = '1235';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/battery')
+              .end((error, response) => {
+                  response.should.have.status(200);
+                  energyResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not found', (done) => {
+          let vehicleID = '1236';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/battery')
+              .end((error, response) => {
+                  response.should.have.status(404);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not valid', (done) => {
+          let vehicleID = 'ash';
+          chai.request(server)
+              .get('/vehicles/' + vehicleID + '/battery')
+              .end((error, response) => {
+                  response.should.have.status(400);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+});
+
+/*
+ * Test the /POST/vehicle/:id/engine route
+ * The route has been tested for one successful case and for three error scenarios
+ */
+
+describe('/POST Executing Engine Action', () => {
+    it('It should execute the specified action on a vehicle (given its ID) and display the appropriate message', (done) => {
+          let vehicleID = '1234';
+          let data = {
+              action : 'START'
+          };
+          chai.request(server)
+              .post('/vehicles/' + vehicleID + '/engine')
+              .send(data)
+              .end((error, response) => {
+                  //console.log(response);
+                  response.should.have.status(200);
+                  response.body.should.be.a('Object');
+
+                  let length = Object.keys(response.body).length;
+
+                  length.should.be.eql(1);
+                  response.body.should.have.property('status');
+
+                  let status = response.body.status;
+
+                  expect(status).to.be.a('string');
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not found', (done) => {
+          let vehicleID = '1236';
+          let data = {
+              action : "START"
+          };
+          chai.request(server)
+              .post('/vehicles/' + vehicleID + '/engine')
+              .send(data)
+              .end((error, response) => {
+                  response.should.have.status(404);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating given vehicle ID is not a valid number', (done) => {
+          let vehicleID = 'ash';
+          let data = {
+              action : "START"
+          };
+          chai.request(server)
+              .post('/vehicles/' + vehicleID + '/engine')
+              .send(data)
+              .end((error, response) => {
+                  response.should.have.status(400);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+    it('It should display an error stating specified action cannot be processed as invalid', (done) => {
+          let vehicleID = '1234';
+          let data = {
+              action : "DUMMY"
+          };
+          chai.request(server)
+              .post('/vehicles/' + vehicleID + '/engine')
+              .send(data)
+              .end((error, response) => {
+                  response.should.have.status(422);
+                  errorResponse(response);
+                  done();
+              });
+    });
+
+});
